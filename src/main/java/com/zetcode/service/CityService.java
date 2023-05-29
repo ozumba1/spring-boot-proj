7061e53964383f1811eeeb3e6a876aa93a327300
ozumba1 <118073041+ozumba1@users.noreply.github.com>
2023-05-28 21:43:01 -0400

    first commit

---
+++ src/main/java/com/zetcode/service/CityService.java
@@ -0,0 +1,22 @@
+package com.zetcode.service;
+
+import com.zetcode.model.City;
+import com.zetcode.repository.CityRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityService implements ICityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Override
+    public List<City> findAll() {
+
+        var cities = (List<City>) repository.findAll();
+
+        return cities;
+    }
+}
